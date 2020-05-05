d9fd1e430727dbb59603a9ca6e5a2fc72dd6a0f6
valmikroy <valmikroy@gmail.com>
2020-05-05 04:07:58 +0000

    Commiting simple tree

--- pkg/bintree/simple.go
+++ pkg/bintree/simple.go
@@ -2,28 +2,28 @@ package bintree
 
 import "fmt"
 
-type Node struct {
-	left  *Node
-	right *Node
+type SimpleNode struct {
+	left  *SimpleNode
+	right *SimpleNode
 	value int
 }
 
-func NewTree(val int) *Node {
-	return &Node{
+func NewSimpleTree(val int) *SimpleNode {
+	return &SimpleNode{
 		left:  nil,
 		right: nil,
 		value: val,
 	}
 }
 
-func (n *Node) Insert(val int) {
+func (n *SimpleNode) Insert(val int) {
 
 	switch {
 
 	// store value is greater than new one then go left
 	case n.value > val:
 		if n.left == nil {
-			n.left = &Node{value: val}
+			n.left = &SimpleNode{value: val}
 		} else {
 			n.left.Insert(val)
 		}
@@ -31,7 +31,7 @@ func (n *Node) Insert(val int) {
 	// store value is less than new one then go right
 	case n.value < val:
 		if n.right == nil {
-			n.right = &Node{value: val}
+			n.right = &SimpleNode{value: val}
 		} else {
 			n.right.Insert(val)
 		}
@@ -39,7 +39,7 @@ func (n *Node) Insert(val int) {
 
 }
 
-func (n *Node) Traverse() {
+func (n *SimpleNode) Traverse() {
 
 	// reach to rightmost value
 	if n.right != nil {
